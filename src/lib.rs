425f562f92f1fd585603a7da8474f69aa8a43600
A4-Tacks <wdsjxhno1001@163.com>
2026-05-01 06:03:33 +0800

    Manual impl rfold

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,14 @@ where
         }
         self.iter.next_back()
     }
+
+    #[inline]
+    fn rfold<B, F>(self, init: B, f: F) -> B
+    where
+        F: FnMut(B, Self::Item) -> B,
+    {
+        self.iter.rfold(init, f)
+    }
 }
 
 impl<I> core::iter::FusedIterator for SizeHint<I> where I: core::iter::FusedIterator {}
